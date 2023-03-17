852aef548d4fc0b431a56b27280eec39063aee28
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-17 12:20:21 -0400

    Display results of game

--- dice.js
+++ dice.js
@@ -1,3 +1,4 @@
+var result_text = document.querySelector(".container h1");
 var img1 = document.querySelector(".dice .img1");
 var img2 = document.querySelector(".dice .img2");
 
@@ -11,3 +12,11 @@ var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
 img1.src = imagesSource + randomDiceImage1;
 img2.src = imagesSource + randomDiceImage2;
+
+if (randomNumber1 > randomNumber2) {
+    result_text.innerHTML = "Player 1 Wins!"
+} else if (randomNumber2 > randomNumber1) {
+    result_text.innerHTML = "Player 2 Wins!"
+} else {
+    result_text.innerHTML = "Draw!"
+}
